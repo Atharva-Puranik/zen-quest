a27bb02961909170305d5e838e9dd9baa6814278
Atharva-Puranik <atharvap887@gmail.com>
2023-12-05 00:53:14 +0530

    Project Import

---
+++ src/main/java/com/atharvapuranik/zenquest/ZenQuestApplication.java
@@ -0,0 +1,13 @@
+package com.atharvapuranik.zenquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atharvapuranik/zenquest/ZenQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atharvapuranik.zenquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
